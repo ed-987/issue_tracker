e8d7397184f9de932fc166d040f8e02059b062d5
ed-987 <e10002001-xx@yahoo.com>
2020-10-12 12:09:16 +0200

    2nd commit

---
+++ src/main/java/com/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomePageController {
+
+	@GetMapping(value= "/")
+	@ResponseBody
+	public String homePage() {
+		return "issue tracker test";
+	}
+}
