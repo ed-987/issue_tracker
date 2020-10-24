e7cd6f1758d06c239b59bff4d8b7dfabc51cfba1
ed-987 <e10002001-xx@yahoo.com>
2020-10-24 20:58:16 +0200

    test updates

--- src/test/java/com/mock_auth/MockMvcOAuth2Test.java
+++ src/test/java/com/mock_auth/MockMvcOAuth2Test.java
@@ -2,11 +2,6 @@ package com.mock_auth;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -15,16 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.security.Auth2Token;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
@@ -35,44 +29,47 @@ public class MockMvcOAuth2Test {
     @Autowired
     private MockMvc mockMvc;
     
+    private ResultActions result;
+    
     private final Logger logger = LoggerFactory.getLogger(getClass());
     
-    private static final String id = "auth0|5f8733ba8c86bc00757e7bc9";
-    private static final String email = "e10002001-0@yahoo.com";
-	 
-
-    @Test
-    public void testGetAuthenticationInfo() throws Exception {
-        OAuth2AuthenticationToken principal = buildPrincipal();
+    public void givenUserIsOnPage(String user, String page) throws Exception {
+        OAuth2AuthenticationToken principal = Auth2Token.buildPrincipal(user);
         MockHttpSession session = new MockHttpSession();
         session.setAttribute(
             HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
             new SecurityContextImpl(principal));
+        result = mockMvc.perform(MockMvcRequestBuilders.get(page)
+                .session(session)); 
+    }
+    
+    @Test
+    public void adminStatusIsOkOnAdminPage() throws Exception {
+    	givenUserIsOnPage("ADMIN","/admin");
+    	result.andExpect(status().isOk());
+    }
+    
+    @Test
+    public void userStatusIsForbiddenOnAdminPage() throws Exception {
+    	givenUserIsOnPage("","/admin");
+    	result.andExpect(status().is4xxClientError());
+    }
+    
+    @Test
+    public void userStatusIsOkOnHomePage() throws Exception {
+    	givenUserIsOnPage("","/");
+    	result.andExpect(status().isOk());
+    }
 
-        String result = mockMvc.perform(MockMvcRequestBuilders.get("/admin")
-            .session(session))
-        	//.andDo(print())
-            .andExpect(status().isOk())
-            .andReturn().getResponse().getContentAsString();
-        
-        logger.info("result from API: {}",result);
-        
-//        mockMvc.perform(MockMvcRequestBuilders.get("/api/user")
-//                .session(session))
+
+//        	  .andDo(print())
+//            .andExpect(status().isOk())
+//            .andReturn().getResponse().getContentAsString();
+//        
+//        logger.info("result from API: {}",result);
+//        
 //            .andExpect(status().isOk())
 //            .andExpect(jsonPath("$.email").value(email))
 //            .andExpect(jsonPath("$.id").value(id));
-    }
 
-    private static OAuth2AuthenticationToken buildPrincipal() {
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put("sub", id);
-        attributes.put("email", email);
-        attributes.put("http://role/", "[\"ADMIN\"]");
-
-        List<GrantedAuthority> authorities = Collections.singletonList(
-                new OAuth2UserAuthority("ROLE_USER", attributes));
-        OAuth2User user = new DefaultOAuth2User(authorities, attributes, "sub");
-        return new OAuth2AuthenticationToken(user, authorities, "whatever");
-    }
 }
